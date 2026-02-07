8c60c407cd11d5310cd2bc0d990fee3c48950a7e
BijonTalukder <bijontalukder1247@gmail.com>
2026-02-08 00:25:56 +0600

    cors enable

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,22 +4,26 @@ import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cors({
-      origin:
-        process.env.NODE_ENV === 'production'
-          ? process.env.CORS_ORIGIN?.split(',')
-          : [
-            'http://localhost:5173',
-            'http://192.168.66.51:3000',
-            'http://localhost:3000',
-            'http://192.168.66.212:3000',
-            'http://192.168.66.19:3000',
-          ],
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-      credentials: true,
-    }),
-  );
+  app.enableCors({
+    origin: '*', // dev phase
+    credentials: true,
+  });
+  // app.use(
+  //   cors({
+  //     origin:
+  //       process.env.NODE_ENV === 'production'
+  //         ? process.env.CORS_ORIGIN?.split(',')
+  //         : [
+  //           'http://localhost:5173',
+  //           'http://192.168.66.51:3000',
+  //           'http://localhost:3000',
+  //           'http://192.168.66.212:3000',
+  //           'http://192.168.66.19:3000',
+  //         ],
+  //     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //     credentials: true,
+  //   }),
+  // );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
